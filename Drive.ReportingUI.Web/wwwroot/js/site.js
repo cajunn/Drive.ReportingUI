5c49b681fa39cd2327ae9db609e3517be313e313
Marco.Leblanc2 <marco.leblanc2@novascotia.ca>
2026-05-11 15:28:27 -0300

    initial work

--- Drive.ReportingUI.Web/wwwroot/js/site.js
+++ Drive.ReportingUI.Web/wwwroot/js/site.js
@@ -1,4 +1,56 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿            //0 <th>Id</th>
+            //1 <th>Event Date</th>
+            //2 <th>Entity Type</th>
+            //3 <th>Event Type</th>
+            //4 <th>Actor Id</th>
+            //5 <th>Entity Data </th>
 
-// Write your JavaScript code.
+
+
+$(function () {
+    const table = $('#businessEventsTable'); 
+
+    if (table.length) {
+        table.DataTable({
+            pageLength: 25, 
+            order: [[1, 'desc']], 
+
+            columnControl: [
+                {
+                    target: 0,
+                    content: ['order']
+                },
+                {
+                    target: 2,
+                    content: ['order', ['searchList']]
+                }, 
+                {
+                    target: 3,
+                    content: ['order', ['searchList']]
+                }
+            ]
+            //layout: {
+            //    top1: {
+            //        searchPanes: {
+            //            cascadePanes: true, 
+            //            viewTotal: true
+            //        }
+            //    }
+            //},
+            //columnDefs: [
+            //    {
+            //        searchPanes: {
+            //            show: true
+            //        }, 
+            //        targets: [2, 3]
+            //    },
+            //    {
+            //        searchPanes: {
+            //            show: false
+            //        },
+            //        targets: '_all'
+            //    }
+            //]
+        });
+    }
+});
